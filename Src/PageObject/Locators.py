4de39248e9e98d8a53ac4c9dd8c0a55cdf72b542
greeyk <greeyk34@gmail.com>
2022-09-27 16:31:58 +0200

    directores

---
+++ Src/PageObject/Locators.py
@@ -0,0 +1,5 @@
+class Locator(object):
+    # Google Search Page
+    search_text = "//input[@name='q'"
+    submit = "//div[@class='gNO89b']//input[@name='btnK']"
+    logo = "//*[@id='hplogo']"
---
+++ Src/PageObject/Pages/HomePage.py
@@ -0,0 +1,22 @@
+import sys
+
+sys.path.append(sys.path[0] + "/....")
+
+from selenium.webdriver.common.by import By
+from Src.PageObject.Locators import Locator
+
+class Home(object):
+    def __init__(self, driver):
+        self.driver = driver
+        self.logo = driver.find_element(By.XPATH, Locator.logo)
+        self.search_text = driver.find_element(By.XPATH, Locator.search_text)
+        self.submit = driver.find_element(By.XPATH, Locator.submit)
+
+    def getSearchText(self):
+        return self.search_text
+
+    def getSubmit(self):
+        return self.submit
+
+    def getWebPageLogo(self):
+        return self.logo
