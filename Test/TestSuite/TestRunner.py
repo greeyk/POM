56572798353da7b835b59aca62e45dc800149589
greeyk <greeyk34@gmail.com>
2022-10-05 21:10:12 +0200

    php travels login with captcha iframe

--- Test/TestSuite/TestRunner.py
+++ Test/TestSuite/TestRunner.py
@@ -22,3 +22,4 @@ if __name__ == "__main__":
 
     parallel_suite = testtools.ConcurrentStreamTestSuite(lambda: ((case, None) for case in test_suite))
     parallel_suite.run(testtools.StreamResult())
+
